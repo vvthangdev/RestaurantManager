b97100545ed6ae7911b59dc240cb7009edaa6585
quocanh <quocanhak57@gmail.com>
2024-10-21 21:45:40 +0700

    "Stage 2 - Initialize"

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
-
+import Contact from "./pages/Contact/Contact";
+import Order from "./pages/Order/Order";
 export default function AppRoutes(){
     return(
         <Routes>
-            <Route path="/" element={<HomePage/>}>
-
-            </Route>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="/contact" element={<Contact/>} />
+            <Route path="/order" element={<Order/>} />
         </Routes>
     )
 }

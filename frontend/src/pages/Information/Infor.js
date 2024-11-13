3454b2f2558a659b1c7b6c4a57b8e6226e6c86e0
Bean Lover <lamvu20004@gmail.com>
2024-11-13 16:27:54 +0700

    sua lai trang infor

--- frontend/src/pages/Information/Infor.js
+++ frontend/src/pages/Information/Infor.js
@@ -3,37 +3,70 @@ import styles from './Infor.module.css';
 function Infor(){
     return(
         <body>
-            <section className = {styles.container}>
-                <div className={styles.aboutus}>
-                <div className={styles.aboutusbox}>
-                    <div className={styles.aboutuscontent}>
-                <h2>About us</h2></div>
-                </div>
-                </div>
-                <div className = {styles.boxinfo}>
-
-
-                    <div className = {styles.box1}>
-                        <div className={styles.boxfood1}></div>
-                        <div className={styles.boxcontent1}>
-                            <p>Đây là món thịt ngon nhất cửa hàng chúng tôi</p>
+            <div className={styles.infor_wrapper}>
+                <div className={styles.infor_content}>
+                    <div className={styles.infor_box } id ='box1'>
+                    <div className={styles.infor_box_left}>
+                        <div className={styles.infor_box_left_title1}>
+                            <p>Quán nhậu nổi tiếng khu BKX</p>
                         </div>
-                    </div>
-                    <div className = {styles.box2}>
-                        <div className={styles.boxfood2}></div>
-                        <div className={styles.boxcontent2}>
-                            <p>Vai ca cac toi het content r </p>
-                        
+                        <div className ={styles.infor_box_left_des}>
+                            <p>Hoho</p>
+                            <span>Description bla bla eh oh eh oh ec ec</span>
                         </div>
-                    </div>
-                    <div className = {styles.box3}>
-                        <div className={styles.boxfood3}></div>
-                        <div className={styles.boxcontent3}>
-                            <p>Nha hang lam an nhu con cac</p>
                         </div>
+                    <div className={styles.infor_box_right}>
+                        <div className={styles.test}>
+                        <div className = {styles.infor_box_right_image}>
+                            <i>
+                            <img src="https://storage.quannhautudo.com/data/thumb_800/Data/images/product/2023/08/202308051006021532.webp" alt = 'ua sao d chen dc v ae'/>
+                            </i>
+                        </div>
+                        </div>
+                        </div>    
                     </div>
-                </div>
-            </section>
+                    <div className={styles.infor_box } id ='box2'>
+                    <div className={styles.infor_box_left}>
+                        <div className={styles.infor_box_left_title1}>
+                            <p>Quán nhậu nổi tiếng khu BKX</p>
+                        </div>
+                        <div className ={styles.infor_box_left_des}>
+                            <p>Hoho</p>
+                            <span>Description bla bla eh oh eh oh ec ec</span>
+                        </div>
+                        </div>
+                    <div className={styles.infor_box_right}>
+                        <div className={styles.test}>
+                        <div className = {styles.infor_box_right_image}>
+                            <i>
+                            <img src="C:\Users\user\Documents\GitHub\RestaurantManager\frontend\src\pages\Contact\bia-hoi-sai-gon-1.jpg" alt = 'ua sao d chen dc v ae'/>
+                            </i>
+                        </div>
+                        </div>
+                        </div>    
+                    </div>
+                    <div className={styles.infor_box } id ='box3'>
+                    <div className={styles.infor_box_left}>
+                        <div className={styles.infor_box_left_title1}>
+                            <p>Quán nhậu nổi tiếng khu BKX</p>
+                        </div>
+                        <div className ={styles.infor_box_left_des}>
+                            <p>Hoho</p>
+                            <span>Description bla bla eh oh eh oh ec ec</span>
+                        </div>
+                        </div>
+                    <div className={styles.infor_box_right}>
+                        <div className={styles.test}>
+                        <div className = {styles.infor_box_right_image}>
+                            <i>
+                            <img src="https://storage.quannhautudo.com/data/thumb_800/Data/images/product/2023/08/202308051006021532.webp" alt = 'ua sao d chen dc v ae'/>
+                            </i>
+                        </div>
+                        </div>
+                        </div>    
+                    </div>
+                    </div>
+            </div>
         </body>
     )
 }
